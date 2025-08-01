cbeccf0d9955ff3f7e2b83878c67d1ace9c4e05f
radhiptsb <radhi.ptsb@gmail.com>
2025-08-01 23:14:11 +0800

    design class diagram

--- src/main/java/com/monopoly_deal/v1/model/DiscardPile.java
+++ src/main/java/com/monopoly_deal/v1/model/DiscardPile.java
@@ -1,10 +1,8 @@
 package com.monopoly_deal.v1.model;
 
 public class DiscardPile {
-
+    
     public void add(Card card) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
     }
     
-}
+}   
