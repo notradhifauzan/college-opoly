d43f1f99da5865f16e0c8227749c9a250fcf4b00
radhiptsb <radhi.ptsb@gmail.com>
2025-08-05 21:11:21 +0800

    card loader utils for reading cards

---
+++ src/main/java/com/monopoly_deal/v1/utils/CardLoader.java
@@ -0,0 +1,33 @@
+package com.monopoly_deal.v1.utils;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.monopoly_deal.v1.model.Card;
+import com.monopoly_deal.v1.model.PropertyCard;
+
+public class CardLoader {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static List<Card> loadAllCards() {
+        List<Card> allCards = new ArrayList<>();
+
+        return allCards;
+    }
+
+    private static <T> List<PropertyCard> loadPropertyCards() {
+        return readCards("/cards/property-card.json", new TypeReference<List<PropertyCard>>() {});
+    }
+
+    private static <T extends Card> List<T> readCards(String path, TypeReference<List<T>> typeRef) {
+        try (InputStream is = CardLoader.class.getResourceAsStream(path)) {
+            return mapper.readValue(is, typeRef);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+}
